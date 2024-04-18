5094f64a5d5a8562ea2444717855d859dc95193b
VennelaReddyBaddam <vennelareddybaddam04@gmail.com>
2024-04-18 14:44:25 -0500

    Intial commit

---
+++ src/test/java/com/example/DMS/DogsManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.DMS;
+/*
+ * @VennelaReddyBaddam
+ */
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
